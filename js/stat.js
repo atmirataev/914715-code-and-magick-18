382c9b985829e1bc4c822d5018a1051bb40e6663
Atmir Ataev <atmirataev@gmail.com>
2019-09-16 10:20:24 +0300

    Исправляет недостатки в д/з №2

--- js/stat.js
+++ js/stat.js
@@ -3,17 +3,28 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
+var SHADOW_GAP = 10; // Тень облака на 10px смещена вниз и вправо
+var GAP = 20; // общий пробел
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
-var PADDING = 20;
-var MARGIN = 50;
+var BAR_MARGIN = 50; // ширина между столбцами
 
+/**
+ * @description создает облако
+ * @param {string} ctx контекст канваса
+ * @param {number} x расположение по горизонтали
+ * @param {number} y расположение по вертикали
+ * @param {string} color цвет
+ */
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+/**
+ * @param {array} arr массив чисел c результатами прохождения игры
+ * @return {number} максимальный результат
+ */
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
@@ -25,33 +36,45 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+/**
+ * @description создает статистику прохождения игры
+ * @param {string} ctx контекст канваса
+ * @param {array} names массив имён игроков
+ * @param {array} times массив чисел c результатами прохождения игры
+ */
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  var maxTime = getMaxElement(times);
+  /**
+   * @return {string} получаем синий цвет с разной насыщенностью
+   */
+  var getRandomBlueColor = function () {
+    var randomSaturation = Math.floor(Math.random() * (100 + 1));
+    return 'hsl(230, ' + randomSaturation + '%, 50%)';
+  };
+
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + PADDING, CLOUD_Y + PADDING);
-  ctx.fillText('Список результатов:', CLOUD_X + PADDING, CLOUD_Y + PADDING + PADDING);
-
-  var maxTime = getMaxElement(times);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + 2 * GAP);
 
   for (var i = 0; i < names.length; i++) {
+    var currentHeight = (BAR_HEIGHT * times[i]) / maxTime; // Высота текущего столбца в пропорции c максимальным столбцом
+
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], CLOUD_X + MARGIN + (BAR_WIDTH + MARGIN) * i, CLOUD_HEIGHT - PADDING);
+    ctx.fillText(names[i], CLOUD_X + BAR_MARGIN + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_HEIGHT - GAP);
 
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = function () {
-        var randomSaturation = Math.floor(Math.random() * (100 - 0 + 1) + 0);
-        return 'hsl(230, ' + randomSaturation + '%, 50%)';
-      }();
+      ctx.fillStyle = getRandomBlueColor();
     }
 
-    ctx.fillRect(CLOUD_X + MARGIN + (BAR_WIDTH + MARGIN) * i, CLOUD_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - PADDING - GAP, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect(CLOUD_X + BAR_MARGIN + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_HEIGHT - currentHeight - GAP, BAR_WIDTH, currentHeight);
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.floor(times[i]), CLOUD_X + MARGIN + (BAR_WIDTH + MARGIN) * i, CLOUD_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - PADDING * 2 - GAP);
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + BAR_MARGIN + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_HEIGHT - currentHeight - GAP * 2);
   }
 };
