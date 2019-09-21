a299443e2aa15600afe7ef7b2ed80cc01b470ea6
Atmir Ataev <atmirataev@gmail.com>
2019-09-21 12:28:05 +0300

    Генерацию волшебника вынесли в отдельную функцию

--- js/setup.js
+++ js/setup.js
@@ -5,8 +5,8 @@ var NUMBER_OF_SIMILAR_WIZARDS = 4;
 var wizardSettings = document.querySelector('.setup');
 wizardSettings.classList.remove('hidden');
 
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURENAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURENAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
@@ -21,15 +21,19 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .querySelector('.setup-similar-item');
 
 var createWizard = function () {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
   var wizard = {
-    name: getRandomElem(NAMES) + ' ' + getRandomElem(SURENAMES),
+    name: getRandomElem(WIZARD_NAMES) + ' ' + getRandomElem(WIZARD_SURENAMES),
     coatColor: getRandomElem(COAT_COLORS),
     eyesColor: getRandomElem(EYES_COLORS)
   };
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizard;
+};
+
+var renderWizard = function () {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = createWizard().name;
+  wizardElement.querySelector('.wizard-coat').style.fill = createWizard().coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = createWizard().eyesColor;
 
   return wizardElement;
 };
@@ -37,7 +41,7 @@ var createWizard = function () {
 var fragment = document.createDocumentFragment();
 
 for (var i = 0; i < NUMBER_OF_SIMILAR_WIZARDS; i++) {
-  fragment.appendChild(createWizard());
+  fragment.appendChild(renderWizard());
 }
 
 similarListElement.appendChild(fragment);
